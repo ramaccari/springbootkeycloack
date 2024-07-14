b4999d8c450857524870f89d035c060a9f43b72d
ramaccari <ramaccari@gmail.com>
2024-07-14 00:20:57 -0300

    Inicio del proyecto

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Título
+
+Spring Boot con Keycloack.
+
+
+## Descripción
+
+Demo de una aplicación construida con Spring Boot cuya seguridad está administrada por Keycloak.
---
+++ src/main/java/com/keycloak/demo/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.keycloak.demo.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/Hello-1")
+	@PreAuthorize("hasRole('admin_client_role')")
+	public String helloAdmin() {
+		return "Hello Spring Boot with Keycloack - ADMIN";
+	}
+
+	@GetMapping("/Hello-2")
+	@PreAuthorize("hasRole('admin_client_role') or hasRole('user_client_role')")
+	public String helloUser() {
+		return "Hello Spring Boot with Keycloack - USER";
+	}
+
+}
